d2420d44bd65a8a09dbd9d31571dee2a686f4f4c
inte <inte.vleminckx14@hotmail.com>
2021-10-08 14:00:35 +0200

    V1.0

---
+++ src/CFG.cpp
@@ -0,0 +1,5 @@
+//
+// Created by inte on 10/8/21.
+//
+
+#include "CFG.h"
---
+++ src/CFG.h
@@ -0,0 +1,21 @@
+//
+// Created by inte on 10/8/21.
+//
+
+#ifndef MB_PROGRAMMEEROPDRACHT_2_CFG_H
+#define MB_PROGRAMMEEROPDRACHT_2_CFG_H
+
+#include <iostream>
+
+
+using namespace std;
+
+class CFG {
+public:
+
+    CFG();
+
+};
+
+
+#endif //MB_PROGRAMMEEROPDRACHT_2_CFG_H
